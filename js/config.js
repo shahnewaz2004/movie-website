e20819f0e739cf3e95180f66a3160088ba650034
Shahnewaz <personal.shahnewazrakib@gmail.com>
2023-02-22 23:50:20 +0600

    Completed

---
+++ js/config.js
@@ -0,0 +1,18 @@
+
+tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            dark: '#100F10',
+            light: '#161616',
+            logo: '#B91D1D'
+          },
+          fontFamily: {
+            inter: ['Inter', 'sans-serif']
+          },
+          backgroundImage: {
+            hero: 'url(./image/batman.jpg)',
+          }
+        } 
+      }
+}
